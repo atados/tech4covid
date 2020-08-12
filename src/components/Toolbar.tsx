dfd754bc800d3fd60883105789aa684c79053b7e
Vinicius Pacheco Furtado <vin175pacheco@gmail.com>
2020-08-12 17:09:18 -0300

    Adjust toolbar links

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -3,7 +3,7 @@ import cx from 'classnames'
 import Link from 'next/link'
 import Logo from 'assets/svg/logo.svg'
 import LogoText from 'assets/svg/new_logo.svg'
-import { FaCode } from 'react-icons/fa'
+import { FaCode, FaUserFriends, FaBookOpen } from 'react-icons/fa'
 
 export interface ToolbarProps {
   readonly joinLink: string
@@ -16,15 +16,32 @@ const Toolbar: React.FC<ToolbarProps> = ({ joinLink, className }) => {
       <Link href="/">
         <a className="flex items-center">
           <Logo height={36} width={60} className="mr-4" />
-          <LogoText width={320} height={35}/>
+          <LogoText width={320} height={35} />
         </a>
       </Link>
-      <div className="flex items-center space-x-8 text-white lg:ml-auto text-md pt-6 lg:pt-0">
-        <a href={joinLink} target="__blank">
+      <div className="flex items-center space-x-8 text-gray-800 lg:ml-auto text-xl pt-6 lg:pt-0">
+        <a
+          href={joinLink}
+          target="__blank"
+          className="hover:underline hover:text-secondary-800 hover:bg-secondary-200"
+        >
+          <FaUserFriends className="inline-block mr-2" />
           Junte-se a nós
         </a>
-        <a className="topBarLink" href="#projetos"><FaCode/><p>Veja os projetos</p></a>
-        {/* <a className="topBarLink" href="#manifesto">Leia o manifesto</a> */}
+        <a
+          href="#projetos"
+          className="hover:underline hover:text-secondary-800 hover:bg-secondary-200"
+        >
+          <FaCode className="inline-block mr-2" />
+          Veja os projetos
+        </a>
+        <a
+          className="hover:underline hover:text-secondary-800 hover:bg-secondary-200"
+          href="#manifesto"
+        >
+          <FaBookOpen className="inline-block mr-2" />
+          Leia o manifesto
+        </a>
       </div>
     </div>
   )
